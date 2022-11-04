ac19991db386fba7760ac8ba24a962a94307c38f
Mateus Costa dos Santos <mtssantos@academicos.com>
2022-11-04 20:28:53 -0300

    Update

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,18 @@
+import './index.css';
+
 const Login = () =>{
     return(
-        <h1>Login!</h1>
+        <div>
+            <div className="uk-text-center form-login">
+                <form action="">
+                    <label htmlFor="femail">E-mail:</label>
+                    <input className=".uk-input" type="email" id="femail" />
+                    <label htmlFor="fpassword">Senha:</label>
+                    <input className=".uk-input" type="password" id="fpassword"/>
+                </form> 
+            </div>
+
+        </div>
     )
 }
 
