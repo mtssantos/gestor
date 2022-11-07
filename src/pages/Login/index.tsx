d4fd2e2af26a34d2e8bf52fb8dff2c901081ce0a
Mateus Santos <mtscostadossantos@gmail.com>
2022-11-07 14:42:45 -0300

    Update Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,17 +1,29 @@
 import './index.css';
+import logo from '/assets/logo.svg';
 
 const Login = () =>{
     return(
-        <div>
-            <div className="uk-text-center form-login">
-                <form action="">
-                    <label htmlFor="femail">E-mail:</label>
-                    <input className=".uk-input" type="email" id="femail" />
-                    <label htmlFor="fpassword">Senha:</label>
-                    <input className=".uk-input" type="password" id="fpassword"/>
-                </form> 
+        <div id="page-login">
+            <div className="content container">
+                <div className="mt-4">
+                    <img src={logo} alt="Logo Atend" className="ounded mx-auto d-block pt-4" />
+                </div>
+                <div className="form-login">
+                    <form action="">
+                        <div className="mb-3">
+                            <label htmlFor="femail">CNPJ:</label>
+                            <input className="form-control" type="text" id="femail" />       
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="fpassword">Senha:</label> 
+                            <input className="form-control" type="password" id="fpassword"/>
+                        </div>
+                        <div id="button-confirm">
+                            <button className="btn btn-primary">Entrar</button>
+                        </div>                        
+                    </form> 
+                </div>
             </div>
-
         </div>
     )
 }
